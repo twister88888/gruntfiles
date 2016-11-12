570fa3f07a62a284e1e1ba0da690af8e60898f1c
Juanma Ortiz <twister88888@gmail.com>
2016-11-12 17:39:45 +0100

    grunt sass

---
+++ gruntfile-sass.js
@@ -0,0 +1,24 @@
+// Nos permite compilar archivos sass
+// https://github.com/sindresorhus/grunt-sass
+module.exports = function (grunt) {
+  'use strict'
+  // Project configuration.
+  require('load-grunt-tasks')(grunt)
+
+  grunt.initConfig({
+    sass: {
+      options: {
+        sourceMap: true,
+        sourceMapEmbed: true,
+        sourceMapContents: true
+      },
+      dist: {
+        files: {
+          'src/static/css/style.css': 'src/scss/style.scss'
+        }
+      }
+    }
+  })
+  // Default task.
+  grunt.registerTask('default', ['sass'])
+}
